f7ab8f80dd757cff73e702a64a8a48c2a10b17fb
Joel <jitousig@gmail.com>
2018-03-17 13:17:19 -0400

    added read data

--- processing.R
+++ processing.R
@@ -1,7 +1,9 @@
 #Drop certain Pitch outcomes
-filtered2<-pitches[!(pitches$outcomeDescription == 'Balk' | pitches$outcomeDescription == 'Catcher Interference' | pitches$outcomeDescription == 'Intentional Ball'),]
+pitches<-readRDS(filtered1.RDS)
+
+pitches<-pitches[!(pitches$outcomeDescription == 'Balk' | pitches$outcomeDescription == 'Catcher Interference' | pitches$outcomeDescription == 'Intentional Ball'),]
 
 #Adding column 'ball' with TRUE or FALSE entries
-filtered2 = mutate(filtered2, ball = outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'))
+pitches = mutate(filtered2, ball = outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'))
 
-filtered2 %>% select(outcomeDescription, ball) 
+#pitches %>% select(outcomeDescription, ball) 
