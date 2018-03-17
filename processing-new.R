31b096fe255c9f877d51443accc114d2a0fc0a8e
Joel <jitousig@gmail.com>
2018-03-17 15:07:16 -0400

    rename is_ball from logical to values

--- processing-new.R
+++ processing-new.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 pitches<-readRDS("filtered1.RDS")
 
 #Drop certain Pitch outcomes
@@ -7,11 +9,14 @@ pitches<-pitches[!(pitches$outcomeDescription == 'Balk' | pitches$outcomeDescrip
 pitches<-pitches[!(pitches$pitchTypeDescription == 'Intentional Ball' | pitches$pitchTypeDescription == 'Knuckleball'),]
 
 #Adding column 'is_ball' with TRUE or FALSE entries
-pitches = mutate(pitches, is_ball = outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'))
+pitches = mutate(pitches, is_ball = ifelse(outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'), "Ball","Strike")
 
 #Adding column 'pitchCategory'
 pitches = mutate(pitches, pitchCategory = ifelse(pitchTypeDescription %in% c('Fastball','Cutter','Sinker', 'Splitter'),'Fastball',ifelse(pitchTypeDescription %in% c('Changeup'),'Changeup','Breaking Ball')))
 
+pitches = mutate(pitches, runner_on_first = !is.na(rob1_start))
+pitches = mutate(pitches, runner_on_second = !is.na(rob2_start))
+pitches = mutate(pitches, runner_on_third = !is.na(rob3_start))
 
 df_outcome <- pitches %>% filter(pitches$is_ab_over == 1)
 df_outcome$atBatOutcome <- ifelse(df_outcome$outs - df_outcome$startingOuts == 1, "Out", "Not out")
@@ -20,3 +25,4 @@ df_outcome <- df_outcome %>% select(gameId, inningNumber, inningHalf, inningHalf
 pitches2 <- left_join(pitches, df_outcome, by = c('gameId','inningNumber','inningHalf','inningHalfEventSequenceNumber'))
 
 saveRDS(pitches2, "pitches.rds")
+
