2536c606357b8add608a57053de1a7131dff9c27
Diane Fenton <diane.fenton@rbc.com>
2018-03-17 17:03:33 -0400

    final version?

--- app.R
+++ app.R
@@ -37,9 +37,9 @@ ui <- fluidPage(titlePanel("2016 Pitch Explorer"),
                               mainPanel(
                                 tabsetPanel(
                                   tabPanel("Summary", plotlyOutput("coolplot")),
-                                  tabPanel("Fastballs", plotlyOutput("fastballs")),
-                                  tabPanel("Breaking Balls", plotlyOutput("breakingballs")),
-                                  tabPanel("Changeups", plotlyOutput("changeups"))
+                                  tabPanel("Fastballs", plotlyOutput("fastballs"), verbatimTextOutput("fastballEV"), plotlyOutput("fastballruns")),
+                                  tabPanel("Breaking Balls", plotlyOutput("breakingballs"), verbatimTextOutput("breakingEV"), plotlyOutput("breakingruns")),
+                                  tabPanel("Changeups", plotlyOutput("changeups"), verbatimTextOutput("changeupEV"), plotlyOutput("changeupruns"))
                                 ))))
 
 
@@ -64,6 +64,24 @@ server <- function(input, output) {
   
   summary_changeup <- reactive({df_changeup() %>% count(is_ball, atBatOutcome) %>% spread(atBatOutcome,n) %>% mutate(s = Out + `Not out`) %>% mutate(totalsum = sum(s), prop_out = Out/totalsum, prop_not_out = `Not out`/totalsum)})
   
+  df_fastball_joel <- reactive({df_fastballs() %>% count(total_runs_to_come) %>% ungroup %>% mutate(totalrows = sum(n)) %>%
+    mutate(Probability = n/totalrows)})
+  
+  df_breaking_joel <- reactive({df_breakingballs() %>% count(total_runs_to_come) %>% ungroup %>% mutate(totalrows = sum(n)) %>%
+      mutate(Probability = n/totalrows)})
+  
+  df_changeup_joel <- reactive({df_changeup() %>% count(total_runs_to_come) %>% ungroup %>% mutate(totalrows = sum(n)) %>%
+      mutate(Probability = n/totalrows)})
+  
+  expected_runs_fastball  <- reactive({df_fastball_joel() %>% mutate(expectedruns = total_runs_to_come*Probability) %>%
+    summarize(mean(expectedruns)) %>% as.numeric()})
+  
+  expected_runs_breaking  <- reactive({df_breaking_joel() %>% mutate(expectedruns = total_runs_to_come*Probability) %>%
+      summarize(mean(expectedruns)) %>% as.numeric()})
+  
+  expected_runs_changeup  <- reactive({df_changeup_joel() %>% mutate(expectedruns = total_runs_to_come*Probability) %>%
+      summarize(mean(expectedruns)) %>% as.numeric()})
+  
   output$coolplot <- renderPlotly({
     plot_ly(summary_summary(), type = "bar", x = ~pitchCategory, y = ~prop_ball, name = "Ball") %>% add_trace(y = ~prop_strike, name = "Strike") %>% layout(xaxis = list(title = "Type of Pitch"), yaxis = list(title = "Proportion of Total Pitches", range = c(0,1)), barmode = 'stack')
   })
@@ -79,6 +97,39 @@ server <- function(input, output) {
   output$changeups <- renderPlotly({
     plot_ly(summary_changeup(), type = "bar", x = ~is_ball, y = ~prop_out, name = "Out") %>% add_trace(y = ~prop_not_out, name = "Not Out") %>% layout(xaxis = list(title = "Outcome of Pitch"), yaxis = list(title = "Proportion of Total Pitches", range = c(0, 1)), barmode = 'stack')
   })
+  
+  output$fastballruns <- renderPlotly({plot_ly(
+    df_fastball_joel(),
+    x= ~total_runs_to_come,
+    y = ~Probability,
+    name = "Distribution of expected runs to come",
+    type = "bar"
+  )})
+  
+  output$breakingruns <- renderPlotly({plot_ly(
+    df_breaking_joel(),
+    x= ~total_runs_to_come,
+    y = ~Probability,
+    name = "Distribution of expected runs to come",
+    type = "bar"
+  )})
+  
+  output$changeupruns <- renderPlotly({plot_ly(
+    df_changeup_joel(),
+    x= ~total_runs_to_come,
+    y = ~Probability,
+    name = "Distribution of expected runs to come",
+    type = "bar"
+  )})
+  
+  output$fastballEV <- renderText({ 
+    paste(expected_runs_fastball(), " more runs are expected to score in this inning")})
+  
+  output$breakingEV <- renderText({ 
+    paste(expected_runs_breaking(), " more runs are expected to score in this inning")})
+  
+  output$changeupEV <- renderText({ 
+    paste(expected_runs_changeup(), " more runs are expected to score in this inning")})
 }
 
 
