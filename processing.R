9b8c8de73e1564b194ca7fb43f338097535b88a7
Joel <jitousig@gmail.com>
2018-03-17 13:19:14 -0400

    is_ball

--- processing.R
+++ processing.R
@@ -4,6 +4,6 @@ pitches<-readRDS(filtered1.RDS)
 pitches<-pitches[!(pitches$outcomeDescription == 'Balk' | pitches$outcomeDescription == 'Catcher Interference' | pitches$outcomeDescription == 'Intentional Ball'),]
 
 #Adding column 'ball' with TRUE or FALSE entries
-pitches = mutate(filtered2, ball = outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'))
+pitches = mutate(filtered2, is_ball = outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'))
 
 #pitches %>% select(outcomeDescription, ball) 
