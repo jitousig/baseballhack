2b17a3c5c0247efa204afdcf20e93b4c34a63b41
Joel <jitousig@gmail.com>
2018-03-17 13:37:41 -0400

    new processing

---
+++ processing-new.R
@@ -0,0 +1,11 @@
+#Drop certain Pitch outcomes
+pitches<-readRDS("filtered1.RDS")
+
+pitches<-pitches[!(pitches$outcomeDescription == 'Balk' | pitches$outcomeDescription == 'Catcher Interference' | pitches$outcomeDescription == 'Intentional Ball'),]
+
+#Adding column 'ball' with TRUE or FALSE entries
+pitches = mutate(pitches, is_ball = outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'))
+
+saveRDS(pitches, "pitches.rds")
+
+#pitches %>% select(outcomeDescription, ball) 
