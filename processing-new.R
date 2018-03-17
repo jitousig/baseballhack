2627beea8f8839e577cdc4d2052f95c1b4f5d34b
Joel <jitousig@gmail.com>
2018-03-17 15:12:44 -0400

    fix typo

--- processing-new.R
+++ processing-new.R
@@ -9,7 +9,7 @@ pitches<-pitches[!(pitches$outcomeDescription == 'Balk' | pitches$outcomeDescrip
 pitches<-pitches[!(pitches$pitchTypeDescription == 'Intentional Ball' | pitches$pitchTypeDescription == 'Knuckleball'),]
 
 #Adding column 'is_ball' with TRUE or FALSE entries
-pitches = mutate(pitches, is_ball = ifelse(outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'), "Ball","Strike")
+pitches = mutate(pitches, is_ball = ifelse(outcomeDescription %in% c('Ball','Dirt Ball','Hit by pitch', 'Pitch Out'), "Ball","Strike"))
 
 #Adding column 'pitchCategory'
 pitches = mutate(pitches, pitchCategory = ifelse(pitchTypeDescription %in% c('Fastball','Cutter','Sinker', 'Splitter'),'Fastball',ifelse(pitchTypeDescription %in% c('Changeup'),'Changeup','Breaking Ball')))
